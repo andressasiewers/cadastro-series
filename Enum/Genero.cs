05ec9b8a0de50eb92211118c63a88b0a0834682e
andressasiewers <andressacsiewers@gmail.com>
2021-06-06 18:58:01 -0300

    First Commit

---
+++ Enum/Genero.cs
@@ -0,0 +1,20 @@
+  
+namespace cadastro_series
+{
+	public enum Genero
+	{
+		Ação = 1,
+		Aventura = 2,
+		Comédia = 3,
+		Documentário = 4,
+		Drama = 5,
+		Espionagem = 6,
+		Faroeste = 7,
+		Fantasia = 8,
+		Ficção_Cientifica = 9,
+		Musical = 10,
+		Romance = 11,
+		Suspense = 12,
+		Terror = 13
+	}
+}
